0f0e698e3b322add83579fb015979f81d8e1603e
Yudi Tan <yudi98@berkeley.edu>
2020-12-07 11:00:00 -0600

    Minor changes to resolve conflict with yuhan's update

--- hyperspectral-painting/src/sketch.js
+++ hyperspectral-painting/src/sketch.js
@@ -62,17 +62,11 @@ function setup() {
   // Initialize the canvas to the size of the screen.
   createCanvas(window.innerWidth, window.innerHeight);
   fillD(width/2);
-  initialization()
   background(bg); // change the background color using a color picker.
   stroke(255)
   line(d.width,0,d.width,d.height)
   // set up yuhan's color picker
-  colorWheel.resize(colorWheelSize,colorWheelSize)
-  image(colorWheel, margin, margin,colorWheelSize,colorWheelSize)
-  image(colorWheel, d.width+margin, margin,colorWheelSize,colorWheelSize)
-  textSize(20)
-  d.noStroke()
-  d.fill(255,255,255,255,255,255)
+  initialization();
   createResetButton()
   createLoadButton()
   
