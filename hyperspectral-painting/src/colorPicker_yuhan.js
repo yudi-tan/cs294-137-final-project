ddc3f5149802a7e44c4e0faa59aaebc0641bfcb1
Yuhan Yang <yuhany1024@gmail.com>
2020-12-07 10:24:57 -0800

    display image

--- hyperspectral-painting/src/colorPicker_yuhan.js
+++ hyperspectral-painting/src/colorPicker_yuhan.js
@@ -1,7 +1,9 @@
 var LRFlag=0 //LRFlag=0:left,=1:right
 var brushSize=25
-var img
+var imgL, imgR
+var rect_height_y
 let LoadButtonLeft
+var imgFlag = true
 
 function ColorPicker(colorWheel_temp, colorWheelSize_temp, bg_temp, margin_temp, d_temp, socket, mousePressed, mx, my, pmx, pmy){
 	// change the variable to be global
@@ -19,7 +21,7 @@ function ColorPicker(colorWheel_temp, colorWheelSize_temp, bg_temp, margin_temp,
 			if(LRFlag==1)
 				brushColorRight = colorWheel.get(xPos-d.width/2+colorWheelSize/2,yPos-d.height+colorWheelSize+margin)
 		}
-		if (yPos<0.75*innerHeight-brushSize/2){
+		if (yPos<rect_height_y-brushSize/2){
 			paintbrushStroke(mx,my,pmx,pmy,brushColorLeft,brushColorRight)
 		}	
 		// as user draws a stroke, we also send those data over socket so other
@@ -40,9 +42,13 @@ function ColorPicker(colorWheel_temp, colorWheelSize_temp, bg_temp, margin_temp,
 			}
 		socket.send(JSON.stringify(payload));
 	}
-	if (img) {
-		image(img, 200, 200,350*img.width/img.height,350);
-		image(img, 200+d.width, 200,350*img.width/img.height,350);
+	if (imgL && imgFlag) {
+		image(imgL, 0, 0,d.width,rect_height_y);
+		if (imgL.width>1){
+			imgR = computeimgR(imgL)
+			image(imgR, d.width, 0,d.width,rect_height_y);
+			imgFlag = false
+		}
 	}
 }
 
@@ -115,10 +121,10 @@ function createLoadButton(){
 
 function displayImg(file){
   if (file.type === 'image') {
-    img = createImg(file.data, '',);
-    img.hide();
+	imgL = loadImage(file.data)
   } else {
-    img = null;
+    imgL = null;
+	imgR = null;
 	}
 }
 
@@ -126,7 +132,7 @@ function paintDrawingAreas() {
 	let rect_margin_x = 0;
 	let rect_width_x = window.innerWidth / 2 - rect_margin_x;
 	let rect_margin_y = 0;
-	let rect_height_y = window.innerHeight * 0.75;
+	rect_height_y = window.innerHeight * 0.75;
 	let c = color(bg_left);
 	fill(c);
 	noStroke();
@@ -136,4 +142,88 @@ function paintDrawingAreas() {
 	fill(c);
 	noStroke();
 	rect(rect_margin_x_right, rect_margin_y, rect_width_x, rect_height_y);
+}
+
+function computeimgR(imgL){
+	console.log(imgL.width)
+	var imgR = createImage(imgL.width, imgL.height);
+	imgR.loadPixels();
+	for (let i = 0; i < imgR.width; i++) {
+		for (let j = 0; j < imgR.height; j++) {
+			// get the pixel value for imgL
+			c=imgL.get(i,j)
+			gp=rgb2rgpb(c[0],c[1],c[2]);
+			console.log(gp)
+			imgR.set(i, j, color(c[0], gp, c[2]));
+		}
+	}
+	imgR.updatePixels();
+	return imgR
+}
+
+function rgb2rgpb(r,g,b){
+      let hsVals = rgbToHsv(r, g, b);
+
+      let h = hsVals[0];
+      let s = hsVals[1];
+      let v = hsVals[2];
+	  [r,g,gp,b]=HSVtoRGGB(h, s, v);
+	  return gp
+}
+
+
+function rgbToHsv(r, g, b) {
+  // r /= 255, g /= 255, b /= 255;
+
+  var max = Math.max(r, g, b),
+    min = Math.min(r, g, b);
+  var h, s, v = max;
+
+  var d = max - min;
+  s = max == 0 ? 0 : d / max;
+
+  if (max == min) {
+    h = 0; // achromatic
+  } else {
+    switch (max) {
+      case r:
+        h = (g - b) / d + (g < b ? 6 : 0);
+        break;
+      case g:
+        h = (b - r) / d + 2;
+        break;
+      case b:
+        h = (r - g) / d + 4;
+        break;
+    }
+
+    h /= 6;
+  }
+
+  return [h * 60, s, v];
+}
+
+function HSVtoRGGB(h, s, v) {
+    var r, g, gp, b, i, f, p, q, t;
+    i = Math.floor(h * 8);
+    f = h * 8 - i;
+    p = v * (1 - s);
+    q = v * (1 - f * s);
+    t = v * (1 - (1 - f) * s);
+    switch (i % 8) {
+      case 0: r = v, g = t, gp = p, b = p; break;
+      case 1: r = q, g = v, gp = p, b = p; break;
+      case 2: r = p, g = v, gp = t, b = p; break;
+      case 3: r = p, g = q, gp = v, b = p; break;
+      case 4: r = p, g = p, gp = v, b = t; break;
+      case 5: r = p, g = p, gp = q, b = v; break;
+      case 6: r = t, g = p, gp = p, b = v; break;
+      case 7: r = v, g = p, gp = p, b = q; break;
+    }
+    return [
+      Math.round(r * 255),
+      Math.round(g * 255),
+      Math.round(gp * 255),
+      Math.round(b * 255)
+    ];
 }
